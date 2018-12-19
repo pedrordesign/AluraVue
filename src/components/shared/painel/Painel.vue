ab6809cac678b15c261c3185ec868e822dd6ed3c
Pedro Ramos <pedro.ramos@trackerup.com.br>
2018-12-19 15:19:52 -0200

    init vue

---
+++ src/components/shared/painel/Painel.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="md-content demo md-theme-demo-light">
+    <div class="demo-content">
+      <div class="md-layout">
+        <div v-for="img of filteredImgs" class="md-layout-item md-medium-size-30 md-small-size-50 md-xsmall-size-90">
+          <div class="md-card md-theme-demo-light">
+            <div class="md-card-header">
+              <div class="md-title">{{img.titulo}}</div>
+            </div>
+            <div class="md-card-content">
+              <img :src="img.url" :alt="img.titulo" style="max-width:100%; max-height:100%" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['imgs'],
+  
+  computed:{
+    filteredImgs
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/shared/painel/Search.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="md-layout md-gutter">
+    <div class="md-layout-item">
+      <md-field>
+        <label>{{searchLabelTitle}}</label>
+        <md-input v-model="search"></md-input>
+      </md-field>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['searchLabelTitle'],
+  data(){
+    return{
+      search: ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
